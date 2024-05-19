81be7e38fa3946d0d82a94d71fb598f4cec6c144
OlegSud <168107881+OlegSud@users.noreply.github.com>
2024-05-19 11:12:18 +0300

    Add files via upload

---
+++ HW FOR.py
@@ -0,0 +1,10 @@
+my_trans = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+for k in my_trans:
+    print('Я узжу на автомобиле марки ', k)
+
+my_trans = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+cars_count = 0
+for k in my_trans:
+    cars_count += 10
+
+print(cars_count)
